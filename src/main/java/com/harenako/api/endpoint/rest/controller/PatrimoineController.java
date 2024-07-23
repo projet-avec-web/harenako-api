bfbd991d189bedb37a7cf349b5c3be13731c0985
Daris02 <hei.raymond.2@gmail.com>
2024-07-23 10:11:13 +0300

    fix: remove last version of patrimoine api

--- src/main/java/com/harenako/api/endpoint/rest/controller/PatrimoineController.java
+++ src/main/java/com/harenako/api/endpoint/rest/controller/PatrimoineController.java
@@ -1,36 +1,18 @@
 package com.harenako.api.endpoint.rest.controller;
 
-import com.harenako.api.endpoint.rest.model.Patrimoine;
-import com.harenako.api.service.PatrimoineService;
-import jakarta.websocket.server.PathParam;
-import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.harenako.api.PojaGenerated;
+
 @RestController
+@PojaGenerated
 @AllArgsConstructor
 public class PatrimoineController {
-  private PatrimoineService service;
 
   @GetMapping("/patrimoines")
-  public ResponseEntity<?> getPatrimoine(
-      @PathParam("page") Integer page, @PathParam("pageSize") Integer pageSize) {
-    return ResponseEntity.ok().body(service.getPatrimoines());
-  }
-
-  @GetMapping("/patrimoines/{nom_patrimoine}")
-  public ResponseEntity<?> getPatrimoineByNom(
-      @PathVariable("nom_patrimoine") String nom_patrimoine) {
-    return ResponseEntity.ok().body(service.getPatrimoineByNom(nom_patrimoine));
-  }
-
-  @PutMapping("/patrimoines")
-  public ResponseEntity<?> crupdatePatrimoine(@RequestBody List<Patrimoine> patrimoines) {
-    return ResponseEntity.ok().body(service.crupdPatrimoines(patrimoines));
+  public String getAllPatrimoines() {
+    return "patrimoines api in implementation";
   }
 }
