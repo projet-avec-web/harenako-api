685ca9eeb656c789dd174c1df82cfea024b35bae
ManoaLahatra <manoalahatra07@gmail.com>
2024-07-25 19:44:34 +0200

    Impl: PossessionService.getPossessions

--- src/main/java/com/harenako/api/service/PossessionService.java
+++ src/main/java/com/harenako/api/service/PossessionService.java
@@ -2,16 +2,31 @@ package com.harenako.api.service;
 
 import com.harenako.api.PojaGenerated;
 import com.harenako.api.endpoint.rest.model.Patrimoine;
+
+import java.util.Collections;
 import java.util.List;
+
+import com.harenako.api.file.BucketComponent;
+import com.harenako.api.file.BucketConf;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.hei.patrimoine.serialisation.Serialiseur;
 
 @Service
 @PojaGenerated
-@NoArgsConstructor
+@AllArgsConstructor
 public class PossessionService {
-  public String getPossessions(String nom_patrimoine) {
-    return "get all possessions: not implemented";
+  private final Serialiseur serialiseur = new Serialiseur();
+  private final BucketComponent bucketComponent;
+  private final BucketConf bucketConf;
+  private final String POSSESSION_KEY = "possessions/";
+  private final PatrimoineService patrimoineService;
+  public List<Possession> getPossessions(String nom_patrimoine) {
+    List<Possession> possessions = patrimoineService
+            .getPatrimoineByNom(nom_patrimoine)
+            .getPossessions();
+    return listToPagedList(possessions);
   }
 
   public String getPossessionByNom(String nom_patrimoine, String nom_possession) {
@@ -24,5 +39,17 @@ public class PossessionService {
 
   public String removePossession(String nom_patrimoine, String nom_possession) {
     return "delete possessions: not implemented";
+  }
+
+  private List<Possession> listToPagedList(
+          List<Possession> possessions,
+          Integer page,
+          Integer pageSize) {
+    var start = (page - 1) * pageSize;
+    var end = Math.min(start + pageSize, possessions.size());
+    if (start >= possessions.size() || start < 0) {
+      return Collections.emptyList();
+    }
+    return possessions.subList(start, end);
   }
 }
